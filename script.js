27b8e50f27e03a86fdc153dc6e6c7057b0771268
boriusho <boriusho@gmail.com>
2021-10-02 13:13:11 +0200

    start up proyect
    Ironhack Proyect Design implementation

---
+++ script.js
@@ -0,0 +1,8 @@
+const hamburger = document.getElementById('hamburger');
+const menu = document.getElementById('main-menu');
+
+hamburger.addEventListener('click', () => {
+  menu.classList.toggle('show');  
+});
+
+
